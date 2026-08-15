46eedfbb4cd5016ce1cbfc21a0faf0f7f521dd3c
Sneha Nikam <snehanikam796@gmail.com>
2026-08-15 13:54:35 +0530

    Implement basic timer functionality
    Added timer functionality with start, stop, and reset methods.

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+let display = 
+  documents.querySelector(".display");
 
+let second = 0;
+let minute = 0;
+let hour = 0;
+
+let timer = null;
+
+function start() {
+  timer = setInterval(function() {
+  seconds++;
+  display.innerHTML = seconds;
+  }, 1000);
+}
+
+function stop(){
+  clearInterval(timer);
+}
+
+function reset(){
+  clearInterval(timer);
+  seconds = 0;
+  display.innerHTML = seconds;
+}
+
+  
